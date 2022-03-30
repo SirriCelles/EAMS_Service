e49686b6501f715918e1fead012270de1f222968
SirriCelles <sirricelles@gmail.com>
2022-03-30 14:52:19 +0100

    add express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
 import 'dotenv/config'; // use this import to access enviroment variables
+import express from 'express';
+
+
+
+const app = express();
+const port  = process.env.PORT;
+
+
+
+
+
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
+);
